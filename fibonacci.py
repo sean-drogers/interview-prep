d428f1331dac5da1d20b7358730063697052c07e
Sean Rogers <srogers2@stevens.edu>
2026-03-30 22:05:48 -0400

    initial

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    x = 0
+    y = 1
+    for i in range(n):
+        print(x)
+        add = x+y
+        x = y
+        y = add
+    
+
+        
+        
+print(fibonacci(12))
+print(fibonacci(7))
